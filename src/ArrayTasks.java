5c0df1307dd853179307456d35ba984a696c74ba
Надежда <nana7305@mail.ru>
2021-10-28 22:11:07 +0300

    arrays

--- src/ArrayTasks.java
+++ src/ArrayTasks.java
@@ -5,6 +5,10 @@ public class ArrayTasks {
         System.out.println(Arrays.toString(even(4)));
         System.out.println(Arrays.toString(even(1)));
         System.out.println(Arrays.toString(even(0)));
+
+        System.out.println(allSimilar(new int[]{0, 0, 1}));
+        System.out.println(allSimilar(new int[]{0, 0}));
+        System.out.println(allSimilar(new int[]{}));
     }
 
     public static int[] even(int n) {
@@ -13,6 +17,18 @@ public class ArrayTasks {
             a[i] = 2*(i + 1);
         }
         return a;
+    }
+
+    public static boolean allSimilar(int[] a) {
+        if (a.length > 1) {
+            int x = a[0];
+            for (int i : a) {
+                if (i != x) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 
