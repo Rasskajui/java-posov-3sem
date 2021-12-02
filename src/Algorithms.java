023da72eedca8df312733d496c0cabc214353c36
Надежда <nana7305@mail.ru>
2021-12-02 15:26:09 +0300

    Algorithms

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Algorithms {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(sieveOfEratosthenes(20)));
-        System.out.println(Arrays.toString(primes(20)));
+        System.out.println(Arrays.toString(primes(73)));
+        printDivider(divider(1));
+        printDivider(divider(5));
+        printDivider(divider(56));
+        printDivider(divider(600));
     }
 
     public static boolean[] sieveOfEratosthenes(int n){
@@ -46,4 +51,39 @@ public class Algorithms {
         }
         return primes;
     }
+
+    public static ArrayList<int[]> divider(int n) {
+        int[] primes = primes((int) Math.pow(n, 0.5));
+        ArrayList<int[]> divList = new ArrayList();
+
+        if (n == 1){
+            divList.add(new int[]{1, 1});
+            return divList;
+        }
+
+        for (int d : primes) {
+            int s = 0;
+            while (n % d == 0) {
+                n = n /= d;
+                s += 1;
+            }
+            if (s != 0) {
+                divList.add(new int[]{d, s});
+            }
+        }
+
+        if (n != 1){
+            divList.add(new int[]{n, 1});
+        }
+
+        return divList;
+    }
+
+    public static void printDivider(ArrayList<int[]> divider){
+        for (int[] x : divider){
+            System.out.print(Arrays.toString(x) + " ");
+        }
+        System.out.println();
+    }
+
 }
