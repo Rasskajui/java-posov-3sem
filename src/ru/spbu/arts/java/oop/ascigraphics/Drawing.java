9a9255b599cfb3c1b78497ae8bd3413a98cf14da
Rasskajui <nana7305@mail.ru>
2022-02-27 21:18:11 +0300

    class asci (added circle, random line and painted a cute little house)

--- src/ru/spbu/arts/java/oop/ascigraphics/Drawing.java
+++ src/ru/spbu/arts/java/oop/ascigraphics/Drawing.java
@@ -45,5 +45,44 @@ public class Drawing {
         this.drawVerticalLine(x2, y1, y2, c);
     }
 
+    public void drawCircle(int xc, int yc, int r, char c){
+        for (int x = 0; x < this.drawing.length; x++){
+            for (int y = 0; y < this.drawing[x].length; y++){
+                if (Math.pow(x - xc, 2) + Math.pow(y - yc, 2) <= Math.pow(r, 2)){
+                    setPoint(x, y, c);
+                }
+            }
+        }
+    }
 
+    public void draw(int x, int y, Drawing d){
+        for (int yr = y; yr > y - d.drawing.length; yr--){
+            for (int xr = x; xr < x + d.drawing[0].length; xr++){
+                setPoint(xr, yr, d.drawing[y - yr][xr - x]);
+            }
+        }
+    }
+
+    public void drawLine(int x1, int y1, int x2, int y2, char c) {
+        int deltaX = Math.abs(x2 - x1);
+        int deltaY = Math.abs(y2 - y1);
+        float error = 0;
+        float deltaErr = (float) (deltaY + 1) / (float) (deltaX + 1);
+        int y = y1;
+        int dirY = y2 - y1;
+        if (dirY > 0){
+            dirY = 1;
+        } else if (dirY < 0) {
+            dirY = -1;
+        }
+        for (int x = x1; x < x2; x++){
+            setPoint(x, y, c);
+            error += deltaErr;
+            if (error > 1.0){
+                y += dirY;
+                error -= 1.0;
+            }
+        }
+
+    }
 }
--- src/ru/spbu/arts/java/oop/ascigraphics/ExperimentsWithDrawing.java
+++ src/ru/spbu/arts/java/oop/ascigraphics/ExperimentsWithDrawing.java
@@ -2,8 +2,8 @@ package ru.spbu.arts.java.oop.ascigraphics;
 
 public class ExperimentsWithDrawing {
     public static void main(String[] args) {
-        var d1 = new Drawing(5, 4, '.');
-        var d2 = new Drawing(7, 7, '.');
+        var d1 = new Drawing(6, 6, 'c');
+        var d2 = new Drawing(10, 10, '.');
         d1.setPoint(2, 3, '/');
         d1.print();
         System.out.println();
@@ -18,5 +18,22 @@ public class ExperimentsWithDrawing {
 
         d2.drawRectangle(3, 3, 5, 5, 'c');
         d2.print();
+
+        d2.drawCircle(5, 5, 3, ',');
+        d2.print();
+
+        d2.draw(5, 5, d1);
+        d2.print();
+
+//        Начало домика
+
+        var house = new Drawing(20, 12, '.');
+        house.drawRectangle(5, 2, 15, 6, 'c');
+        house.drawCircle(10, 4, 1, '0');
+        house.drawLine(3, 6, 11, 9, '/');
+        house.drawLine(11, 9, 17, 5, '\\');
+
+        house.print();
+
     }
 }
