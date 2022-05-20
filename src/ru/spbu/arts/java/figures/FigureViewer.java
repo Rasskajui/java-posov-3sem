25b2192eae7a58cae57b217e80de2a47931f55f6
Rasskajui <nana7305@mail.ru>
2022-05-20 16:20:22 +0300

    added fractal navigation buttons

--- src/ru/spbu/arts/java/figures/FigureViewer.java
+++ src/ru/spbu/arts/java/figures/FigureViewer.java
@@ -1,32 +1,135 @@
 package ru.spbu.arts.java.figures;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class FigureViewer extends Application {
     public final static int W = 600;
     public final static int H = 500;
 
+    private double x0 = -4.5;
+    private double y0 = 3.75;
+    private double d = 8/600.0;
+
+    private Display display;
+    private Mandelbrot mandelbrot;
+    private WritableImage img;
+
+    private Button left;
+    private Button right;
+    private Button up;
+    private Button down;
+
+    private Button further;
+    private Button closer;
+
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Фракталы");
         Parent parent = initInterface();
+        initInteraction();
+
         stage.setScene(new Scene(parent));
         stage.show();
     }
 
     private Parent initInterface() {
-        WritableImage img = new WritableImage(W, H);
-        Display display = new Display(-4, 5, 8/600.0);
-        Fractal mandelbrot = new Mandelbrot();
+        img = new WritableImage(W, H);
+        display = new Display(x0, y0, d);
+        mandelbrot = new Mandelbrot();
         display.paint(img, mandelbrot);
 
         ImageView imageView = new ImageView(img);
-        return new Pane(imageView);
+        HBox.setHgrow(imageView, Priority.ALWAYS);
+
+        VBox panel = new VBox();
+        HBox bottomPane = new HBox();
+
+        // кнопки навигации
+        left = new Button("<");
+        right = new Button(">");
+        up = new Button("^");
+        down = new Button("˅");
+
+        VBox navPane = new VBox();
+        HBox notUp = new HBox();
+        notUp.getChildren().addAll(left, down, right);
+        navPane.getChildren().addAll(up, notUp);
+        navPane.setAlignment(Pos.CENTER);
+
+        // приближение-отдаление
+        closer = new Button("+");
+        further = new Button("-");
+        HBox scalePane = new HBox();
+        scalePane.getChildren().addAll(further, closer);
+        scalePane.setAlignment(Pos.CENTER);
+
+        // сохранить-загрузить-экспорт
+        Button save = new Button("save");
+        Button load = new Button("load");
+        Button export = new Button("export");
+        HBox actionPane = new HBox();
+        actionPane.getChildren().addAll(save, load, export);
+        actionPane.setAlignment(Pos.CENTER);
+        actionPane.setSpacing(5);
+
+        bottomPane.getChildren().addAll(navPane, scalePane, actionPane);
+        bottomPane.setStyle("-fx-background-color: blue;");
+        bottomPane.setPrefHeight(70);
+        HBox.setHgrow(bottomPane, Priority.ALWAYS);
+        bottomPane.setSpacing(50);
+
+        panel.getChildren().addAll(imageView, bottomPane);
+        return panel;
+    }
+
+    private void initInteraction() {
+        left.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            x0 -= W/30.0 * d;
+            display = new Display(x0, y0, d);
+            display.paint(img, mandelbrot);
+        });
+        right.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            x0 += W/30.0 * d;
+            display = new Display(x0, y0, d);
+            display.paint(img, mandelbrot);
+        });
+        up.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            y0 += H/30.0 * d;
+            display = new Display(x0, y0, d);
+            display.paint(img, mandelbrot);
+        });
+        down.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            y0 -= H/30.0 * d;
+            display = new Display(x0, y0, d);
+            display.paint(img, mandelbrot);
+        });
+        closer.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            double dd = d - d/1.5;
+            x0 += 0.5 * W * dd;
+            y0 -= 0.5 * H * dd;
+            d = d/1.5;
+            display = new Display(x0, y0, d);
+            display.paint(img, mandelbrot);
+        });
+        further.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            double dd = d - d*1.5;
+            x0 += 0.5 * W * dd;
+            y0 -= 0.5 * H * dd;
+            d = d*1.5;
+            display = new Display(x0, y0, d);
+            display.paint(img, mandelbrot);
+        });
+
     }
 }
